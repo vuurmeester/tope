d306e14c5c718409b5c1b4f712fa3556c44cc9a6
demaestro <>
2025-09-19 09:15:40 +0200

    refactored allocator

--- src/allocator.h
+++ src/allocator.h
@@ -14,21 +14,20 @@ typedef struct _Block Block;
 
 typedef struct _Allocator {
   u32 blocksize;
-  void* pool_freeptrs[ALLOCATOR_NPOOLS];
-  u16 sizes[ALLOCATOR_NPOOLS];
-  u8 indices[ALLOCATOR_MAXSIZE / ALLOCATOR_MINSIZE]; /* 8, 16, ..., 512 */
+  Block* pool_freeptrs[ALLOCATOR_NPOOLS];
+  u8 pool_sizes[ALLOCATOR_NPOOLS];
   Block* curblock;
-  void* curblock_freeptr;
+  Block* curblock_freeptr;
 } Allocator;
 
 /** New allocator object. */
 void allocator_init(Allocator* alc);
 
 /** Allocate single unit. */
-void* allocator_alloc(Allocator* alc, u32 numbytes);
+Block* allocator_alloc(Allocator* alc, u16 numbytes);
 
 /** Release memory. */
-void allocator_free(Allocator* alc, void* mem, u32 numbytes);
+void allocator_free(Allocator* alc, Block* mem, u16 numbytes);
 
 /** Release the allocator object, and all memory still associated with it. */
 void allocator_clear(Allocator* alc);
