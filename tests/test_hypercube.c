66b7481784651872fdba3aaa2c4901d480d64c46
vuurmeester <>
2026-01-20 17:46:29 +0100

    small refactor

--- tests/test_hypercube.c
+++ tests/test_hypercube.c
@@ -13,23 +13,22 @@
 
 int main()
 {
+  /* Define a hypercube: */
   double points[NPOINTS * DIM];
-  Tope* tope;
-
-  /* Define a cube. */
   for (int i = 0; i < NPOINTS; ++i) {
     for (int j = 0; j < DIM; ++j) {
       points[i * DIM + j] = ((i >> (DIM - 1 - j)) & 1) - 0.5;
     }
   }
 
-  /* Initialize tope with all but the last: */
-  tope = tope_frompoints(NPOINTS, DIM, points);
+  /* Create tope: */
+  Tope* tope = tope_frompoints(NPOINTS, DIM, points);
   tope_merge(tope);
 
   /* Print the tope: */
   tope_print(tope);
 
+  /* Check: */
   if (tope_getnumfacets(tope) != 2 * DIM) {
     return -1;
   }
